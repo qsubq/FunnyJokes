3ee633a120f3fdeb78c6860df5b9c095417d71d4
FataLity2328 <maksim-kargin@bk.ru>
2022-05-11 16:07:03 +0300

    added background 1

--- app/src/main/java/com/github/qsubq/funnyjockes/MainActivity.kt
+++ app/src/main/java/com/github/qsubq/funnyjockes/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.github.qsubq.funnyjockes
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.github.qsubq.funnyjockes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        val animationDrawable : AnimationDrawable = binding.mainLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2500)
+        animationDrawable.setExitFadeDuration(5000)
+        animationDrawable.start()
     }
 }
